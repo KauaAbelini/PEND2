e5d173a64335aed3be97f459e70317aec62f4e79
Kauã Abelini <kaua.carlota@aluno.senai.br>
2026-09-11 11:06:56 -0300

    atividade prática pt2

--- JavaScript/API/Atividade Prática/script.js
+++ JavaScript/API/Atividade Prática/script.js
@@ -45,3 +45,24 @@ navigator.mediaDevices.getUserMedia({
 .catch(function(error) {
     console.error("Erro ao acessar a câmera:", error);
 });
+
+//================================ FOTO =======================================//
+const canvas = document.querySelector("#canvas");
+const contexto = canvas.getContext("2d");
+const botao = document.querySelector("#botao");
+const foto = document.querySelector("#foto");
+
+botao.addEventListener("click", function() {
+    canvas.width = video.clientWidth;
+    canvas.height = video.clientHeight;
+
+    contexto.drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    foto.src = canvas.toDataURL("image/png");
+});
--- JavaScript/API/Camera - Exemplo/script.js
+++ JavaScript/API/Camera - Exemplo/script.js
@@ -1,10 +1,32 @@
+const video = document.querySelector("#camera");
+const canvas = document.querySelector("#canvas");
+const contexto = canvas.getContext("2d");
+const botao = document.querySelector("#botao");
+const foto = document.querySelector("#foto");
+
 navigator.mediaDevices.getUserMedia({
-    video: true, audio: true
+    video: true,
+    audio: true
 })
 .then(function(stream) {
-    const video = document.querySelector("#camera");
     video.srcObject = stream;
 })
 .catch(function(error) {
     console.error("Erro ao acessar a câmera:", error);
+});
+
+botao.addEventListener("click", function() {
+
+    canvas.width = video.clientWidth;
+    canvas.height = video.clientHeight;
+
+    contexto.drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    foto.src = canvas.toDataURL("image/png");
 });
